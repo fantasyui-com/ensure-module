4fc45ca539952b767f269e215d8e28939d85d0fa
fantasyui-com <captain@fantasyui.com>
2019-07-13 17:06:03 -0400

    Improve Cache

--- index.js
+++ index.js
@@ -7,14 +7,21 @@ module.exports = function(options){
     templateSource: path.resolve(`${__dirname}/templates/`),
     destinationRoot: path.resolve(`./code_modules`),
   }
-  const setup = Object.assign({},options,defaults);
+  const setup = Object.assign({}, defaults, options );
+
+  // console.log(options);
+  // console.log(setup);
 
   return function(name, templateType = 'promise'){
     if( (!fs.existsSync(path.resolve(`${setup.destinationRoot}/${kebabCase(name)}`))) ){
-      fs.mkdirSync(path.resolve(`${setup.destinationRoot}/${kebabCase(name)}`), {recursive:true})
+      fs.mkdirSync(path.resolve(`${setup.destinationRoot}/${kebabCase(name)}/util`), {recursive:true})
 
       fs.readdirSync(`${setup.templateSource}/${templateType}`).forEach(function(childItemName) {
         fs.copyFileSync(`${setup.templateSource}/${templateType}/${childItemName}`, `${setup.destinationRoot}/${kebabCase(name)}/${childItemName}`);
+      });
+
+      fs.readdirSync(`${setup.templateSource}/${templateType}/util`).forEach(function(childItemName) {
+        fs.copyFileSync(`${setup.templateSource}/${templateType}/util/${childItemName}`, `${setup.destinationRoot}/${kebabCase(name)}/util/${childItemName}`);
       });
 
       let p = JSON.parse(fs.readFileSync(`${setup.destinationRoot}/${kebabCase(name)}/package.json`).toString());
